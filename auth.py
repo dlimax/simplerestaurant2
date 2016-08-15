f4ff740c2e0439d95d7e5728f0cdaf3174f6c550
du2x <dudumonteiro@gmail.com>
2016-08-15 19:27:56 -0300

    modified auth.py

--- auth.py
+++ auth.py
@@ -27,7 +27,7 @@ try:
 except IOError:
 	import random
 	auth_app.config['SECRET_KEY'] = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
-	secret = file('secret_key.txt', 'w')
+	secret = open('secret_key.txt', 'w')
 	secret.write(auth_app.config['SECRET_KEY'])
 	secret.close()
 
